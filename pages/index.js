916e4e43323e012a31a5cd81ae4b53cd1dcf1c08
kev-inc <kevinchanxy@gmail.com>
2022-04-06 06:16:31 +0000

    fix classname

--- pages/index.js
+++ pages/index.js
@@ -15,20 +15,20 @@ export default function Home() {
 
             <main className='flex justify-center'>
                 <form className='container p-4' onSubmit={submit}>
-                    <div class="mb-6 flex justify-center">
+                    <div className="mb-6 flex justify-center">
                         <h3 className='text-3xl'>Whatsapp Anyone</h3>
                     </div>
-                    <div class="mb-6 flex justify-center">
+                    <div className="mb-6 flex justify-center">
                         <input 
                             name="number"
                             type="number" 
                             id="number" 
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full max-w-sm p-2.5 text-center" 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full max-w-sm p-2.5 text-center" 
                             placeholder="Whatsapp Phone Number" 
                             required 
                         />
                     </div>
-                    <div class="mb-6 text-center">
+                    <div className="mb-6 text-center">
                         <input type='submit' className='focus:outline-none text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2'/>
                     </div>
                 </form>
